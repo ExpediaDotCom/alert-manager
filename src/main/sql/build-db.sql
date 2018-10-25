e58c8e94f9eeb82f25df8440bfb4513dc462559f
dinilatgit <dinilatgit@users.noreply.github.com>
2018-10-25 20:45:40 +1000

    Update build-db.sql
    update owner field and unique

--- src/main/sql/build-db.sql
+++ src/main/sql/build-db.sql
@@ -8,11 +8,13 @@ create table subscription (
   id          int unsigned primary key not null auto_increment,
   type        char(50)				not null,
   endpoint    char(100)             not null,
+  owner 	  varchar(40),
   details	  json,
   enabled 	  bool                  not null default 1,
   name 		  varchar(100),
   description varchar(100),
   metric_id    char(36),
   detector_id  char(36)        not null,
-  date_created  timestamp          	NULL         default CURRENT_TIMESTAMP
+  date_created  timestamp          	NULL         default CURRENT_TIMESTAMP,
+  unique index (endpoint, metric_id, detector_id)
  );

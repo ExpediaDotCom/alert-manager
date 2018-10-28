c4047b2439fb610d199324d547be6b1cfe70472a
Karan Shah <kashah@expedia.com>
2018-10-29 00:02:37 +0530

    added cors filter

--- src/main/java/com/expedia/alertmanager/AlertManagerApplication.java
+++ src/main/java/com/expedia/alertmanager/AlertManagerApplication.java
@@ -17,11 +17,32 @@ package com.expedia.alertmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class AlertManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AlertManagerApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("PATCH");
+        config.addAllowedMethod("DELETE");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
